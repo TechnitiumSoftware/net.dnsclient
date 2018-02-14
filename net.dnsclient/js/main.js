89841ca960611b5abce5c371a540c702ba0dc958
Shreyas Zare <shreyas@technitium.com>
2018-02-14 20:44:42 +0530

    redirect to https implemented via web.config rewrite rules

--- net.dnsclient/js/main.js
+++ net.dnsclient/js/main.js
@@ -1,9 +1,5 @@
 ﻿
 $(function () {
-    if (location.protocol != 'https:') {
-        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-    }
-
     $("#header").html("<div class=\"title\"><a href=\"/\"><img src=\"/img/logo25x25.png\" alt=\"Technitium Logo\" /><span class=\"text\" style=\"color: #ffffff;\">Technitium</span></a></div>");
     $("#footer").html("<div class=\"content\"><a href=\"https://technitium.com\" target=\"_blank\">Technitium</a> | <a href=\"http://blog.technitium.com\" target=\"_blank\">Blog</a> | <a href=\"https://technitium.com/dns/\" target=\"_blank\">DNS Server</a> | <a href=\"https://github.com/TechnitiumSoftware/net.dnsclient\" target=\"_blank\"><i class=\"fa fa-github\"></i>&nbsp;GitHub</a> | <a href=\"https://technitium.com/aboutus.html\" target=\"_blank\">About</a></div>");
 });
