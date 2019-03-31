b1338d7be72b41bd163217115c83ae2f367ec63f
Shreyas Zare <shreyas@technitium.com>
2019-04-01 00:15:24 +0530

    added .NETCore app

---
+++ net.dnsclient.NETCore/Startup.cs
@@ -0,0 +1,107 @@
+﻿/*
+Technitium dnsclient.net
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using TechnitiumLibrary.Net.Dns;
+
+namespace net.dnsclient.NETCore
+{
+    public class Startup
+    {
+        const bool PREFER_IPv6 = false;
+        const DnsTransportProtocol PROTOCOL = DnsTransportProtocol.Udp;
+        const DnsTransportProtocol RECURSIVE_RESOLVE_PROTOCOL = DnsTransportProtocol.Udp;
+        const int RETRIES = 2;
+        const int TIMEOUT = 2000;
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                HttpRequest Request = context.Request;
+                HttpResponse Response = context.Response;
+
+                if (Request.Path == "/api/dnsclient/")
+                {
+                    try
+                    {
+                        string server = Request.Query["server"];
+                        string domain = Request.Query["domain"];
+                        DnsResourceRecordType type = (DnsResourceRecordType)Enum.Parse(typeof(DnsResourceRecordType), Request.Query["type"]);
+
+                        if (domain.EndsWith("."))
+                            domain = domain.Substring(0, domain.Length - 1);
+
+                        DnsDatagram dnsResponse;
+
+                        if (server == "recursive-resolver")
+                        {
+                            dnsResponse = DnsClient.RecursiveResolve(domain, type, new SimpleDnsCache(), null, PREFER_IPv6, PROTOCOL, RETRIES, TIMEOUT, RECURSIVE_RESOLVE_PROTOCOL);
+                        }
+                        else
+                        {
+                            NameServerAddress nameServer = new NameServerAddress(server);
+
+                            if (nameServer.IPEndPoint == null)
+                            {
+                                nameServer.ResolveIPAddress(null, null, PREFER_IPv6, PROTOCOL, RETRIES, TIMEOUT);
+                            }
+                            else if (nameServer.DomainEndPoint == null)
+                            {
+                                try
+                                {
+                                    nameServer.ResolveDomainName(null, null, PREFER_IPv6, PROTOCOL, RETRIES, TIMEOUT);
+                                }
+                                catch
+                                { }
+                            }
+
+                            dnsResponse = (new DnsClient(nameServer) { PreferIPv6 = PREFER_IPv6, Protocol = PROTOCOL, Retries = RETRIES, Timeout = TIMEOUT, RecursiveResolveProtocol = RECURSIVE_RESOLVE_PROTOCOL }).Resolve(domain, type);
+                        }
+
+                        string jsonResponse = JsonConvert.SerializeObject(dnsResponse, new StringEnumConverter());
+
+                        Response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+                        await Response.WriteAsync("{\"status\":\"ok\", \"response\":" + jsonResponse + "}");
+                    }
+                    catch (Exception ex)
+                    {
+                        string jsonResponse = JsonConvert.SerializeObject(ex);
+
+                        Response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+                        await Response.WriteAsync("{\"status\":\"error\", \"response\":" + jsonResponse + "}");
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ net.dnsclient.NETCore/wwwroot/js/dnsclient.js
@@ -0,0 +1,144 @@
+﻿$(function () {
+
+    $('.dropdown-menu').on('click', 'a', function (e) {
+        e.preventDefault();
+        $(this).closest('.dropdown').find('input').val($(this).text());
+    });
+
+    $("#btnResolve").click(function () {
+
+        var btn = $(this).button('loading');
+
+        var server = $("#txtServer").val();
+        var domain = $("#txtDomain").val();
+        var type = $("#optType").val();
+
+        {
+            var i = server.indexOf("(");
+            if (i > -1) {
+                var j = server.indexOf(")");
+                server = server.substring(i + 1, j);
+            }
+        }
+
+        if ((server === null) || (server === "")) {
+            showAlert("warning", "Missing!", "Please enter a valid DNS server.");
+            btn.button('reset');
+            return;
+        }
+
+        if ((domain === null) || (domain === "")) {
+            showAlert("warning", "Missing!", "Please enter a domain name to query.");
+            btn.button('reset');
+            return;
+        }
+        else {
+            var i = domain.indexOf("://");
+            if (i > -1) {
+                var j = domain.indexOf(":", i + 3);
+
+                if (j < 0)
+                    j = domain.indexOf("/", i + 3);
+
+                if (j > -1)
+                    domain = domain.substring(i + 3, j);
+                else
+                    domain = domain.substring(i + 3);
+
+                $("#txtDomain").val(domain);
+            }
+        }
+
+        window.location.hash = encodeURI($("#txtServer").val()) + "/" + domain + "/" + type;
+
+        var apiUrl = "/api/dnsclient/?server=" + server + "&domain=" + domain + "&type=" + type;
+        var divOutput = $("#divOutput");
+
+        //show loader
+        divOutput.html("<pre><img class='center-block' src='/img/loader.gif' /></pre>");
+        divOutput.show();
+        hideAlert();
+
+        $.ajax({
+            type: "GET",
+            url: apiUrl,
+            dataType: 'json',
+            cache: false,
+            success: function (responseJSON, status, jqXHR) {
+
+                switch (responseJSON.status) {
+                    case "ok":
+                        divOutput.html("<pre>" + JSON.stringify(responseJSON.response, null, 2) + "</pre>");
+                        break;
+
+                    case "error":
+                        showAlert("danger", "Error!", responseJSON.response.Message);
+                        divOutput.hide();
+                        break;
+
+                    default:
+                        showAlert("danger", "Error!", "Invalid status code was received.");
+                        divOutput.hide();
+                        break;
+                }
+
+                btn.button('reset');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                showAlert("danger", "Error!", jqXHR.status + " " + jqXHR.statusText);
+                divOutput.hide();
+                btn.button('reset');
+            }
+        });
+
+        //add server name to list if doesnt exists
+        var txtServerName = $("#txtServer").val();
+        var containsServer = false;
+
+        $("ul.dropdown-menu a").each(function () {
+            if ($(this).html() === txtServerName)
+                containsServer = true;
+        });
+
+        if (!containsServer)
+            $("ul.dropdown-menu").prepend('<li><a href="#">' + txtServerName + '</a></li>');
+    });
+
+    //read hash values at doc ready
+    {
+        if (window.location.hash.length > 0) {
+            var values = window.location.hash.substr(1).split("/");
+            if (values.length === 3) {
+                $("#txtServer").val(decodeURI(values[0]));
+                $("#txtDomain").val(values[1]);
+                $("#optType").val(values[2]);
+
+                $("#btnResolve").click();
+            }
+        }
+    }
+});
+
+function showAlert(type, title, message) {
+    var alertHTML = "<div class=\"alert alert-" + type + "\" role=\"alert\">\
+    <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>\
+    <strong>" + title + "</strong>&nbsp;" + message + "\
+    </div>";
+
+    var divAlert = $(".AlertPlaceholder");
+
+    divAlert.html(alertHTML);
+    divAlert.show();
+
+    if (type === "success") {
+        setTimeout(function () {
+            hideAlert();
+        }, 5000);
+    }
+
+    return true;
+}
+
+function hideAlert() {
+    $(".AlertPlaceholder").hide();
+}
