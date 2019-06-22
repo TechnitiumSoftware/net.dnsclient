02793815203841b919b6aa0dd9e1070e0755fae9
Shreyas Zare <shreyas@technitium.com>
2019-06-22 15:31:05 +0530

    webapp: implemented custom protocol option feature.

--- net.dnsclient.NETCore/wwwroot/js/dnsclient.js
+++ net.dnsclient.NETCore/wwwroot/js/dnsclient.js
@@ -1,8 +1,47 @@
-﻿$(function () {
+﻿/*
+Technitium dnsclient.net
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+$(function () {
 
     $('.dropdown-menu').on('click', 'a', function (e) {
         e.preventDefault();
-        $(this).closest('.dropdown').find('input').val($(this).text());
+
+        var itemText = $(this).text();
+        $(this).closest('.dropdown').find('input').val(itemText);
+
+        if ((itemText.indexOf("TLS") !== -1) || (itemText.indexOf(":853") !== -1))
+            $("#optProtocol").val("TLS");
+        else if (itemText.indexOf("HTTPS-JSON") !== -1)
+            $("#optProtocol").val("HttpsJson");
+        else if ((itemText.indexOf("HTTPS") !== -1) || (itemText.indexOf("http://") !== -1) || (itemText.indexOf("https://") !== -1))
+            $("#optProtocol").val("Https");
+        else {
+            switch ($("#optProtocol").val()) {
+                case "UDP":
+                case "TCP":
+                    break;
+
+                default:
+                    $("#optProtocol").val("UDP");
+                    break;
+            }
+        }
     });
 
     $("#btnResolve").click(function () {
@@ -10,16 +49,23 @@
         var btn = $(this).button('loading');
 
         var server = $("#txtServer").val();
+
+        if (server.indexOf("recursive-resolver") !== -1)
+            $("#optProtocol").val("UDP");
+
         var domain = $("#txtDomain").val();
         var type = $("#optType").val();
+        var protocol = $("#optProtocol").val();
 
         {
             var i = server.indexOf("(");
             if (i > -1) {
-                var j = server.indexOf(")");
+                var j = server.lastIndexOf(")");
                 server = server.substring(i + 1, j);
             }
         }
+
+        server = server.trim();
 
         if ((server === null) || (server === "")) {
             showAlert("warning", "Missing!", "Please enter a valid DNS server.");
@@ -49,9 +95,9 @@
             }
         }
 
-        window.location.hash = encodeURI($("#txtServer").val()) + "/" + domain + "/" + type;
+        window.location.hash = encodeURIComponent($("#txtServer").val()) + "/" + encodeURIComponent(domain) + "/" + type + "/" + protocol;
 
-        var apiUrl = "/api/dnsclient/?server=" + server + "&domain=" + domain + "&type=" + type;
+        var apiUrl = "/api/dnsclient/?server=" + server + "&domain=" + domain + "&type=" + type + "&protocol=" + protocol;
         var divOutput = $("#divOutput");
 
         //show loader
@@ -108,10 +154,15 @@
     {
         if (window.location.hash.length > 0) {
             var values = window.location.hash.substr(1).split("/");
-            if (values.length === 3) {
-                $("#txtServer").val(decodeURI(values[0]));
-                $("#txtDomain").val(values[1]);
+            if (values.length >= 3) {
+                $("#txtServer").val(decodeURIComponent(values[0]));
+                $("#txtDomain").val(decodeURIComponent(values[1]));
                 $("#optType").val(values[2]);
+
+                if (values.length === 4)
+                    $("#optProtocol").val(values[3]);
+                else
+                    $("#optProtocol").val("UDP");
 
                 $("#btnResolve").click();
             }
--- net.dnsclient.NETCore/wwwroot/js/main.js
+++ net.dnsclient.NETCore/wwwroot/js/main.js
@@ -1,4 +1,22 @@
-﻿
+﻿/*
+Technitium dnsclient.net
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 $(function () {
     $("#header").html("<div class=\"title\"><a href=\"/\"><img src=\"/img/logo25x25.png\" alt=\"Technitium Logo\" /><span class=\"text\" style=\"color: #ffffff;\">Technitium</span></a></div>");
     $("#footer").html("<div class=\"content\"><a href=\"https://technitium.com\" target=\"_blank\">Technitium</a> | <a href=\"https://blog.technitium.com\" target=\"_blank\">Blog</a> | <a href=\"https://technitium.com/dns/\" target=\"_blank\">DNS Server</a> | <a href=\"https://github.com/TechnitiumSoftware/net.dnsclient\" target=\"_blank\"><i class=\"fa fa-github\"></i>&nbsp;GitHub</a> | <a href=\"https://technitium.com/aboutus.html\" target=\"_blank\">About</a></div>");
