08a118fab3c7090082b23f1a49b63a5aaac79a0f
Shreyas Zare <shreyas@technitium.com>
2020-05-23 18:07:30 +0530

    minor update

--- net.dnsclient.NETCore/Startup.cs
+++ net.dnsclient.NETCore/Startup.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium dnsclient.net
-Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -134,7 +134,7 @@ namespace net.dnsclient.NETCore
 
                                 dnsResponse = dnsClient.Resolve(domain, type);
 
-                                if (dnsResponse.Header.Truncation && (dnsClient.Protocol == DnsTransportProtocol.Udp))
+                                if (dnsResponse.Truncation && (dnsClient.Protocol == DnsTransportProtocol.Udp))
                                 {
                                     dnsClient.Protocol = DnsTransportProtocol.Tcp;
                                     dnsResponse = dnsClient.Resolve(domain, type);
