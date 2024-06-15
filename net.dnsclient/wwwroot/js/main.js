1b429f360fe694893f699929b3d5d2f87a8976c1
Shreyas Zare <shreyas@technitium.com>
2024-06-15 17:37:00 +0530

    main.js: updated code to use current location for home link.

--- net.dnsclient/wwwroot/js/main.js
+++ net.dnsclient/wwwroot/js/main.js
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium dnsclient.net
-Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2024  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,6 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 $(function () {
-    $("#header").html("<div class=\"title\"><a href=\"/\"><img src=\"/img/logo25x25.png\" alt=\"Technitium Logo\" /><span class=\"text\" style=\"color: #ffffff;\">Technitium</span></a></div>");
+    $("#header").html("<div class=\"title\"><a href=\"" + window.location.href + "\"><img src=\"/img/logo25x25.png\" alt=\"Technitium Logo\" /><span class=\"text\" style=\"color: #ffffff;\">Technitium</span></a></div>");
     $("#footer").html("<div class=\"content\"><a href=\"https://technitium.com\" target=\"_blank\">Technitium</a> | <a href=\"https://blog.technitium.com\" target=\"_blank\">Blog</a> | <a href=\"https://technitium.com/dns/\" target=\"_blank\">DNS Server</a> | <a href=\"https://github.com/TechnitiumSoftware/net.dnsclient\" target=\"_blank\"><i class=\"fa fa-github\"></i>&nbsp;GitHub</a> | <a href=\"https://technitium.com/aboutus.html\" target=\"_blank\">About</a></div>");
 });
