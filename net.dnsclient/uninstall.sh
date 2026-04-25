bb712290ca807c59117554346e501515479b75d0
Shreyas Zare <shreyas@technitium.com>
2026-04-25 13:22:35 +0530

    uninstall.sh: Updated script to support non-root service uninstallation. Added prompt to ask before removing dotnet.

--- net.dnsclient/uninstall.sh
+++ net.dnsclient/uninstall.sh
@@ -4,6 +4,8 @@ dotnetDir="/opt/dotnet"
 
 dnsClientDir="/opt/technitium/dnsclient"
 
+serviceUser="dns-client"
+
 echo ""
 echo "================================="
 echo "Technitium DNS Client Uninstaller"
@@ -13,22 +15,36 @@ echo "Uninstalling Technitium DNS Client..."
 
 if [ -d $dnsClientDir ]
 then
-	if [ "$(ps --no-headers -o comm 1 | tr -d '\n')" = "systemd" ] 
-	then
-		sudo systemctl disable dnsclient.service >/dev/null 2>&1
-		sudo systemctl stop dnsclient.service >/dev/null 2>&1
-		rm /etc/systemd/system/dnsclient.service >/dev/null 2>&1
-	fi
+    if [ "$(ps --no-headers -o comm 1 | tr -d '\n')" = "systemd" ] 
+    then
+        sudo systemctl disable dnsclient.service >/dev/null 2>&1
+        sudo systemctl stop dnsclient.service >/dev/null 2>&1
+        rm /etc/systemd/system/dnsclient.service >/dev/null 2>&1
 
-	rm -rf $dnsClientDir >/dev/null 2>&1
+        userdel -f $serviceUser >/dev/null 2>&1
+    fi
 
-	if [ -d $dotnetDir ]
-	then
-		echo "Uninstalling .NET Runtime..."
-		rm /usr/bin/dotnet >/dev/null 2>&1
-		rm -rf $dotnetDir >/dev/null 2>&1
-	fi
+    rm -rf $dnsClientDir >/dev/null 2>&1
+
+    if [ -d $dotnetDir ]
+    then
+        echo ""
+        printf "Do you want to uninstall .NET Runtime (Y/n): "
+        read -r answer0 < /dev/tty
+
+        case "$answer0" in
+            [Nn]* )
+                echo ".NET Runtime was not uninstalled."
+                ;;
+            * )
+                echo "Uninstalling .NET Runtime..."
+                rm /usr/bin/dotnet >/dev/null 2>&1
+                rm -rf $dotnetDir >/dev/null 2>&1
+                ;;
+        esac
+    fi
 fi
 
 echo ""
 echo "Thank you for using Technitium DNS Client!"
+echo ""
