48180ef31d9c0bd2990d5ac49d8106db53a7f6b8
Shreyas Zare <shreyas@technitium.com>
2026-07-11 15:41:05 +0530

    uninstall.sh: updated script to support alpine linux and openrc service.

--- net.dnsclient/uninstall.sh
+++ net.dnsclient/uninstall.sh
@@ -10,11 +10,12 @@ echo ""
 echo "================================="
 echo "Technitium DNS Client Uninstaller"
 echo "================================="
-echo ""
-echo "Uninstalling Technitium DNS Client..."
 
 if [ -d $dnsClientDir ]
 then
+    echo ""
+    echo "Uninstalling Technitium DNS Client..."
+
     if [ "$(ps --no-headers -o comm 1 | tr -d '\n')" = "systemd" ] 
     then
         sudo systemctl disable dnsclient.service >/dev/null 2>&1
@@ -22,27 +23,34 @@ then
         rm /etc/systemd/system/dnsclient.service >/dev/null 2>&1
 
         userdel -f $serviceUser >/dev/null 2>&1
-    fi
+    elif [ -x "/sbin/rc-service" ]
+    then
+        rc-service dnsclient stop  >/dev/null 2>&1
+        rc-update del dnsclient  >/dev/null 2>&1
+        rm /etc/init.d/dnsclient >/dev/null 2>&1
+        
+        deluser $serviceUser >/dev/null 2>&1
+    fi 2>/dev/null
 
     rm -rf $dnsClientDir >/dev/null 2>&1
+fi
 
-    if [ -d $dotnetDir ]
-    then
-        echo ""
-        printf "Do you want to uninstall .NET Runtime (Y/n): "
-        read -r answer0 < /dev/tty
+if [ -d $dotnetDir ]
+then
+    echo ""
+    printf "Do you want to uninstall .NET Runtime (Y/n): "
+    read -r answer0 < /dev/tty
 
-        case "$answer0" in
-            [Nn]* )
-                echo ".NET Runtime was not uninstalled."
-                ;;
-            * )
-                echo "Uninstalling .NET Runtime..."
-                rm /usr/bin/dotnet >/dev/null 2>&1
-                rm -rf $dotnetDir >/dev/null 2>&1
-                ;;
-        esac
-    fi
+    case "$answer0" in
+        [Nn]* )
+            echo ".NET Runtime was not uninstalled."
+            ;;
+        * )
+            echo "Uninstalling .NET Runtime..."
+            rm /usr/bin/dotnet >/dev/null 2>&1
+            rm -rf $dotnetDir >/dev/null 2>&1
+            ;;
+    esac
 fi
 
 echo ""
